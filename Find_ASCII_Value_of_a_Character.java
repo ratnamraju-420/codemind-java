eae489a7a604df07f12e717328700cd39498413e
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-09-17 18:15:10 +0530

    import java.util.*; public class asci {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         char ch = sc.next().charAt(0);         int a = ch;         System.out.println(a);
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class asci
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int a = ch;
+        System.out.println(a);
+        
+    }
+}
