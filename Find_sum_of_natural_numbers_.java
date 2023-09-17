29d56ecee13b71d3e545b039ede5523a40f444ea
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-09-17 18:35:10 +0530

    import java.util.*; public class raju { 	public static void main(String[] args) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		n=(n*(n+1))/2; 		System.out.println(n); 	} }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class raju
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		n=(n*(n+1))/2;
+		System.out.println(n);
+	}
+}
